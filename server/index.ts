e0dd74509522a71589d181493dace4bb6008cf0c
Vandana Yadav <vandanayadav0163@gmail.com>
2026-02-11 11:30:51 +0530

    Fixed Render port binding

--- server/index.ts
+++ server/index.ts
@@ -90,13 +90,17 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "127.0.0.1",
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  // httpServer.listen(
+  //   {
+  //     port,
+  //     host: "127.0.0.1",
+  //   },
+  //   () => {
+  //     log(`serving on port ${port}`);
+  //   },
+  // );
+  httpServer.listen(port, "0.0.0.0", () => {
+  log(`serving on port ${port}`);
+});
+
 })();
